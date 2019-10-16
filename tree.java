03c1796697aa7bfd137f822ce4ccdf8da7ed4a2b
danihott <56000624+danihott@users.noreply.github.com>
2019-10-16 14:35:41 -0500

    Update tree.java
    Updated version for updated question 1

--- tree.java
+++ tree.java
@@ -1,6 +1,16 @@
 public class tree {
     private node root;
 
+    public boolean isConnectionFound() {
+        return connectionFound;
+    }
+
+    public void setConnectionFound(boolean connectionFound) {
+        this.connectionFound = connectionFound;
+    }
+
+    private boolean connectionFound = false;
+
     public tree(node root) {
         this.root = root;
     }
